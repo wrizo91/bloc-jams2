b95f4135581315a0a839c9998bb5f8b16f3f824d
wrizo91 <wrizo91@gmail.com>
2017-06-29 19:00:44 -0500

    checkpoint 17

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,4 +1,7 @@
- var collectionItemTemplate =
+ // #1
+ var buildCollectionItemTemplate = function() {
+     var template =
+         
      '<div class="collection-album-container column fourth">'
    + '  <img src="assets/images/album_covers/01.png"/>'
    + '  <div class="collection-album-info caption">'
@@ -13,15 +16,22 @@
    + '  </div>'
    + '</div>'
    ;
-
- window.onload = function() {
-     // #1
-     var collectionContainer = document.getElementsByClassName('album-covers')[0];
+ 
      // #2
-     collectionContainer.innerHTML = '';
+     return $(template);
+ };
+
+  $(window).load(function() {
+     // #3
+     var $collectionContainer = $('.album-covers');
+     // #4
+     $collectionContainer.empty();
  
      // #3
      for (var i = 0; i < 12; i++) {
-         collectionContainer.innerHTML += collectionItemTemplate;
+
+         var $newThumbnail = buildCollectionItemTemplate();
+         // #5
+         $collectionContainer.append($newThumbnail);         
      }
- }
+ });
